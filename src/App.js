1dcf48b8b131b6b5f764f94c45a7f39fcd2a6ade
reloadercf <elcfm1@gmail.com>
2020-05-31 19:14:22 -0500

    Layouts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 import React from 'react';
-import Hello from './components/Hello'
+import './app.css'
+import { Layout, Menu } from 'antd';
+import {
+  BarChartOutlined,
+  UserOutlined,
+  UploadOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import Hello from'./components/Hello'
+
+const { Header, Content, Footer, Sider } = Layout;
 
 function App() {
     return ( 
-    <div>
-         <Hello / >
-    </div>
+        <div>
+             <Layout>
+                <Sider
+                style={{
+                    overflow: 'auto',
+                    height: '100vh',
+                    position: 'fixed',
+                    left: 0,
+                }}
+                >
+                <div className="logo" />
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['3']}>
+                    <Menu.Item key="1" icon={<UserOutlined />}>
+                    Desayunos
+                    </Menu.Item>
+                    <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                    Comida
+                    </Menu.Item>
+                    <Menu.Item key="3" icon={<UploadOutlined />}>
+                    Ordenes pendientes
+                    </Menu.Item>
+                    <Menu.Item key="4" icon={<BarChartOutlined />}>
+                    Historial
+                    </Menu.Item>
+                </Menu>
+                </Sider>
+                <Layout className="site-layout" style={{ marginLeft: 200 }}>
+                <Header className="site-layout-background" style={{ padding: 0,paddingLeft:30 }} >Burger Queen</Header>
+                <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                    <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
+                        <Hello />
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Carlos Mendoza ©2020 Created for Laboratoria</Footer>
+                </Layout>
+            </Layout>
+        </div>
     );
 }
 
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,9 +1,10 @@
 import React,{Fragment} from 'react';
-
 function Hello() {
     return (
         <Fragment>
+
             <h1>Hola mundo</h1>
+            
         </Fragment>
     );
 }
